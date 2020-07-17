bc57164cc761542af7b38b683f7ab43c9d401dbd
Varnesh Gawde <gawdevarnesh@gmail.com>
2020-07-17 10:31:10 +0530

    Create Reverse_Integer.py

---
+++ Reverse_Integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x=0:
+          return 0
+        if x<0:
+            x=-int(str(-x)[::-1])
+        else:
+            x=int(str(x)[::-1])
+        return x if x<=(2**31-1) and x>=-(2**31) else 0
